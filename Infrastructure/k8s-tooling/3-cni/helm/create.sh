3c1f7224677483becf159073192decae99c41072
mariomerco <mario.mercado.c@gmail.com>
2023-05-02 18:59:17 -0500

    create script added here

--- Infrastructure/k8s-tooling/3-cni/helm/create.sh
+++ Infrastructure/k8s-tooling/3-cni/helm/create.sh
@@ -1,7 +1,24 @@
-base_domain=$(aws route53 list-hosted-zones --query "HostedZones[0].Name" --output text | rev | cut -c2- | rev)
+subnet_a=`aws cloudformation describe-stack-resources --stack-name secondary-subnets --query "StackResources[?LogicalResourceId=='SubnetA'].PhysicalResourceId" --output text`
+subnet_b=`aws cloudformation describe-stack-resources --stack-name secondary-subnets --query "StackResources[?LogicalResourceId=='SubnetB'].PhysicalResourceId" --output text`
+subnet_c=`aws cloudformation describe-stack-resources --stack-name secondary-subnets --query "StackResources[?LogicalResourceId=='SubnetC'].PhysicalResourceId" --output text`
+
+sg1=`aws eks describe-cluster --name eks-acg --query "cluster.resourcesVpcConfig.securityGroupIds[0]" --output text`
+sg2=`aws eks describe-cluster --name eks-acg --query "cluster.resourcesVpcConfig.clusterSecurityGroupId" --output text`
+
+region='us-east-1'
 
 helm upgrade --install \
-    --namespace development \
-    --create-namespace \
-    --set baseDomain=${base_domain} \
-    clients-api-development .
+    --namespace kube-system \
+    --set az1.name=${region}a \
+    --set az1.sg1=${sg1} \
+    --set az1.sg2=${sg2} \
+    --set az1.subnetId=${subnet_a} \
+    --set az2.name=${region}b \
+    --set az2.sg1=${sg1} \
+    --set az2.sg2=${sg2} \
+    --set az2.subnetId=${subnet_b} \
+    --set az3.name=${region}c \
+    --set az3.sg1=${sg1} \
+    --set az3.sg2=${sg2} \
+    --set az3.subnetId=${subnet_c} \
+    vpc-cni-subnet-crds .
--- Infrastructure/k8s-tooling/3-cni/setup.sh
+++ Infrastructure/k8s-tooling/3-cni/setup.sh
@@ -27,27 +27,3 @@ aws cloudformation deploy \
 kubectl set env ds aws-node -n kube-system AWS_VPC_K8S_CNI_CUSTOM_NETWORK_CFG=true
 kubectl set env ds aws-node -n kube-system ENI_CONFIG_LABEL_DEF=failure-domain.beta.kubernetes.io/zone
 
-subnet_a=`aws cloudformation describe-stack-resources --stack-name secondary-subnets --query "StackResources[?LogicalResourceId=='SubnetA'].PhysicalResourceId" --output text`
-subnet_b=`aws cloudformation describe-stack-resources --stack-name secondary-subnets --query "StackResources[?LogicalResourceId=='SubnetB'].PhysicalResourceId" --output text`
-subnet_c=`aws cloudformation describe-stack-resources --stack-name secondary-subnets --query "StackResources[?LogicalResourceId=='SubnetC'].PhysicalResourceId" --output text`
-
-sg1=`aws eks describe-cluster --name eks-acg --query "cluster.resourcesVpcConfig.securityGroupIds[0]" --output text`
-sg2=`aws eks describe-cluster --name eks-acg --query "cluster.resourcesVpcConfig.clusterSecurityGroupId" --output text`
-
-region='us-east-1'
-
-helm upgrade --install \
-    --namespace kube-system \
-    --set az1.name=${region}a \
-    --set az1.sg1=${sg1} \
-    --set az1.sg2=${sg2} \
-    --set az1.subnetId=${subnet_a} \
-    --set az2.name=${region}b \
-    --set az2.sg1=${sg1} \
-    --set az2.sg2=${sg2} \
-    --set az2.subnetId=${subnet_b} \
-    --set az3.name=${region}c \
-    --set az3.sg1=${sg1} \
-    --set az3.sg2=${sg2} \
-    --set az3.subnetId=${subnet_c} \
-    vpc-cni-subnet-crds .
