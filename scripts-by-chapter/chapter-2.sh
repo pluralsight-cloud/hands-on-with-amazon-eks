0c44cd67350364c6d373486fe19d653db49f9add
mariomerco <mario.mercado.c@gmail.com>
2023-05-07 16:14:07 -0500

    logical order for the chapter

--- scripts-by-chapter/chapter-2.sh
+++ scripts-by-chapter/chapter-2.sh
@@ -4,30 +4,32 @@ echo "***************************************************"
 echo "********* CHAPTER 2 - STARTED AT $(date) **********"
 echo "***************************************************"
 echo "--- This could take around 10 minutes"
-#  Create the DynamoDB Tables
-    ( cd ./clients-api/infra/cloudformation && ./create-dynamodb-table.sh development )
-    ( cd ./inventory-api/infra/cloudformation && ./create-dynamodb-table.sh development )
-    ( cd ./renting-api/infra/cloudformation && ./create-dynamodb-table.sh development )
-    ( cd ./resource-api/infra/cloudformation && ./create-dynamodb-table.sh development )
 
 # Getting NodeGroup IAM Role from Kubernetes Cluster
     nodegroup_iam_role=$(aws cloudformation list-exports --query "Exports[?contains(Name, 'nodegroup-eks-node-group::InstanceRoleARN')].Value" --output text | xargs | cut -d "/" -f 2)
-
-# Adding DynamoDB Permissions to the node
-    aws iam attach-role-policy --role-name ${nodegroup_iam_role} --policy-arn arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess
-
-# Installing ExternalDNS
-    ./Infrastructure/k8s-tooling/1-external-dns/create.sh
-    aws iam attach-role-policy --role-name ${nodegroup_iam_role} --policy-arn arn:aws:iam::aws:policy/AmazonRoute53FullAccess
 
 # Installing Load Balancer Controller
     ( cd ./Infrastructure/k8s-tooling/2-load-balancer-controller && ./create.sh )
     aws_lb_controller_policy=$(aws cloudformation describe-stacks --stack-name aws-load-balancer-iam-policy --query "Stacks[*].Outputs[?OutputKey=='IamPolicyArn'].OutputValue" --output text | xargs)
     aws iam attach-role-policy --role-name ${nodegroup_iam_role} --policy-arn ${aws_lb_controller_policy}
 
-
 # Create SSL Certfiicate in ACM
     ( cd ./Infrastructure/cloudformation/ssl-certificate && ./create.sh )
+
+# Installing ExternalDNS
+    ./Infrastructure/k8s-tooling/1-external-dns/create.sh
+    aws iam attach-role-policy --role-name ${nodegroup_iam_role} --policy-arn arn:aws:iam::aws:policy/AmazonRoute53FullAccess
+
+#  Create the DynamoDB Tables
+    ( cd ./clients-api/infra/cloudformation && ./create-dynamodb-table.sh development )
+    ( cd ./inventory-api/infra/cloudformation && ./create-dynamodb-table.sh development )
+    ( cd ./renting-api/infra/cloudformation && ./create-dynamodb-table.sh development )
+    ( cd ./resource-api/infra/cloudformation && ./create-dynamodb-table.sh development )
+
+
+
+# Adding DynamoDB Permissions to the node
+    aws iam attach-role-policy --role-name ${nodegroup_iam_role} --policy-arn arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess
 
 
 # Installing the applications
