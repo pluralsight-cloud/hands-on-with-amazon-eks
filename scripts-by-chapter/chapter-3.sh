a19adf65055b5ddb7105a05718d94e7d79c98799
mariomerco <mario.mercado.c@gmail.com>
2023-05-31 15:23:01 -0500

    chapter 3

--- scripts-by-chapter/chapter-3.sh
+++ scripts-by-chapter/chapter-3.sh
@@ -2,7 +2,7 @@ echo "***************************************************"
 echo "********* CHAPTER 3 - STARTED AT $(date) **********"
 echo "***************************************************"
 
-
+eksctl utils associate-iam-oidc-provider --cluster=eks-acg --approve
 
 echo "*************************************************************"
 echo "********* READY FOR CHAPTER 4 - FINISHED AT $(date) *********"
