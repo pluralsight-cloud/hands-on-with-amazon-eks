777da81fefb235bd8865d51867510fdf8fe6d484
mariomerco <mario.mercado.c@gmail.com>
2023-05-31 16:15:16 -0500

    adding code for creating sa

--- scripts-by-chapter/chapter-3.sh
+++ scripts-by-chapter/chapter-3.sh
@@ -5,8 +5,37 @@ echo "***************************************************"
 echo "********* CHAPTER 3 - STARTED AT $(date) **********"
 echo "***************************************************"
 
-eksctl utils associate-iam-oidc-provider --cluster=eks-acg
-eksctl utils associate-iam-oidc-provider --cluster=eks-acg --approve
+# Create OIDC Provider and connect it with EKS
+    eksctl utils associate-iam-oidc-provider --cluster=eks-acg --approve
+
+# Create IAM Policies of Bookstore Microservices
+    ( cd clients-api/infra/cloudformation && ./create-iam-policy.sh )
+    ( cd resource-api/infra/cloudformation && ./create-iam-policy.sh )
+    ( cd inventory-api/infra/cloudformation && ./create-iam-policy.sh )
+    ( cd renting-api/infra/cloudformation && ./create-iam-policy.sh )
+
+# Create IAM Service Accounts
+    resource_iam_policy=$(aws cloudformation describe-stacks --stack development-iam-policy-resource-api --query "Stacks[0].Outputs[0]" | jq .OutputValue | tr -d '"')
+    renting_iam_policy=$(aws cloudformation describe-stacks --stack development-iam-policy-renting-api --query "Stacks[0].Outputs[0]" | jq .OutputValue | tr -d '"')
+    inventory_iam_policy=$(aws cloudformation describe-stacks --stack development-iam-policy-inventory-api --query "Stacks[0].Outputs[0]" | jq .OutputValue | tr -d '"')
+    clients_iam_policy=$(aws cloudformation describe-stacks --stack development-iam-policy-clients-api --query "Stacks[0].Outputs[0]" | jq .OutputValue | tr -d '"')
+    eksctl create iamserviceaccount --name resources-api-iam-service-account \
+        --namespace development \
+        --cluster eks-acg \
+        --attach-policy-arn ${resource_iam_policy} --approve
+    eksctl create iamserviceaccount --name renting-api-iam-service-account \
+        --namespace development \
+        --cluster eks-acg \
+        --attach-policy-arn ${renting_iam_policy} --approve
+    eksctl create iamserviceaccount --name inventory-api-iam-service-account \
+        --namespace development \
+        --cluster eks-acg \
+        --attach-policy-arn ${inventory_iam_policy} --approve
+    eksctl create iamserviceaccount --name clients-api-iam-service-account \
+        --namespace development \
+        --cluster eks-acg \
+        --attach-policy-arn ${clients_iam_policy} --approve
+
 
 echo "*************************************************************"
 echo "********* READY FOR CHAPTER 4 - FINISHED AT $(date) *********"
