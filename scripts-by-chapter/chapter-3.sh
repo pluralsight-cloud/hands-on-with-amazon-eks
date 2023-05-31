bbf57c4d5ab60ae4028d0401c0765668b37580cc
mariomerco <mario.mercado.c@gmail.com>
2023-05-31 16:45:07 -0500

    detaching dynamodb resources

--- scripts-by-chapter/chapter-3.sh
+++ scripts-by-chapter/chapter-3.sh
@@ -14,6 +14,12 @@ echo "***************************************************"
     ( cd inventory-api/infra/cloudformation && ./create-iam-policy.sh )
     ( cd renting-api/infra/cloudformation && ./create-iam-policy.sh )
 
+# Getting NodeGroup IAM Role from Kubernetes Cluster
+    nodegroup_iam_role=$(aws cloudformation list-exports --query "Exports[?contains(Name, 'nodegroup-eks-node-group::InstanceRoleARN')].Value" --output text | xargs | cut -d "/" -f 2)
+
+# Removing DynamoDB Permissions to the node
+    aws iam detach-role-policy --role-name ${nodegroup_iam_role} --policy-arn arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess
+
 # Create IAM Service Accounts
     resource_iam_policy=$(aws cloudformation describe-stacks --stack development-iam-policy-resource-api --query "Stacks[0].Outputs[0]" | jq .OutputValue | tr -d '"')
     renting_iam_policy=$(aws cloudformation describe-stacks --stack development-iam-policy-renting-api --query "Stacks[0].Outputs[0]" | jq .OutputValue | tr -d '"')
@@ -35,6 +41,12 @@ echo "***************************************************"
         --namespace development \
         --cluster eks-acg \
         --attach-policy-arn ${clients_iam_policy} --approve
+
+# Upgrading the applications
+    ( cd ./resource-api/infra/helm-v2 && ./create.sh )
+    ( cd ./clients-api/infra/helm-v2 && ./create.sh )
+    ( cd ./inventory-api/infra/helm-v2 && ./create.sh )
+    ( cd ./renting-api/infra/helm-v2 && ./create.sh )
 
 
 echo "*************************************************************"
