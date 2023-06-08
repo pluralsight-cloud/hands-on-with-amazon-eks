21d54148a3897b3d74517a27c430ce02cf70ecab
mariomerco <mario.mercado.c@gmail.com>
2023-06-08 18:30:54 -0500

    external dns irsa

---
+++ Infrastructure/k8s-tooling/external-dns/create-irsa.sh
@@ -0,0 +1,12 @@
+helm repo add external-dns https://kubernetes-sigs.github.io/external-dns/
+
+service_account_name="external-dns-service-account"
+
+eksctl create iamserviceaccount --name ${service_account_name} \
+    --cluster eks-acg \
+    --attach-policy-arn arn:aws:iam::aws:policy/AmazonRoute53FullAccess --approve
+
+helm upgrade --install external-dns \
+    --set serviceAccount.create=false \
+    --serviceAccount.name=${service_account_name} \
+    external-dns/external-dns
--- scripts-by-chapter/chapter-3.sh
+++ scripts-by-chapter/chapter-3.sh
@@ -52,7 +52,16 @@ echo "***************************************************"
 # Updating IRSA for AWS Load Balancer Controller
     
     helm del -n kube-system aws-load-balancer-controller # Uninstall first
+    aws_load_balancer_iam_policy=$(aws cloudformation describe-stacks --stack aws-load-balancer-iam-policy --query "Stacks[0].Outputs[0]" | jq .OutputValue | tr -d '"')
+    aws iam detach-role-policy --role-name ${nodegroup_iam_role} --policy-arn ${aws_load_balancer_iam_policy}
     ( cd ./Infrastructure/k8s-tooling/load-balancer-controller && ./create-irsa.sh )
+
+# Updating IRSA for External DNS
+    
+    helm del external-dns # Uninstall first
+    external_dns_iam_policy="arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
+    aws iam detach-role-policy --role-name ${nodegroup_iam_role} --policy-arn ${external_dns_iam_policy}
+    ( cd ./Infrastructure/k8s-tooling/external-dns && ./create-irsa.sh )
 
 
 echo "*************************************************************"
