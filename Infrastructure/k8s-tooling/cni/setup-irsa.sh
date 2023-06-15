5d420846a0709faaa51424b6de4e99e26ac93632
mariomerco <mario.mercado.c@gmail.com>
2023-06-15 17:11:26 -0500

    commenting the eksctl for the vpc-cni

--- Infrastructure/k8s-tooling/cni/setup-irsa.sh
+++ Infrastructure/k8s-tooling/cni/setup-irsa.sh
@@ -1,14 +1,16 @@
 service_account_name="vpc-cni-service-account"
-iam_role_name="vpc-cni-iam-role"
+iam_role_name="aws-node"
 aws_account_id=$(aws sts get-caller-identity | jq .Account | tr -d '"')
 
 eksctl create iamserviceaccount --name ${service_account_name} \
     --namespace kube-system \
     --cluster eks-acg \
     --role-name ${iam_role_name} \
-    --attach-policy-arn arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy --approve
+    --attach-policy-arn arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy \
+    --override-existing-serviceaccounts \
+    --approve
 
-eksctl update addon \
-    --name vpc-cni \
-    --cluster eks-acg \
-    --service-account-role-arn arn:aws:iam::${aws_account_id}:role/${iam_role_name}
+# eksctl update addon \
+#     --name vpc-cni \
+#     --cluster eks-acg \
+#     --service-account-role-arn arn:aws:iam::${aws_account_id}:role/${iam_role_name}
