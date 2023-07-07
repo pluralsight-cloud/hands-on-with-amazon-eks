69d4da6585395cd7cf721ae0d8e7f14a48f4868e
mariomerco <mario.mercado.c@gmail.com>
2023-07-07 15:03:12 -0500

    starting with chapter 5

--- front-end/infra/helm-v3/create.sh
+++ front-end/infra/helm-v3/create.sh
@@ -1,7 +1,14 @@
+environment=${1}
+
+if [ -z "$environment" ]; then
+    echo "No environment specified. Using 'development'"
+    environment='development'
+fi
+
 base_domain=$(aws route53 list-hosted-zones --query "HostedZones[0].Name" --output text | rev | cut -c2- | rev)
 
 helm upgrade --install \
-    --namespace development \
+    --namespace ${environment} \
     --create-namespace \
     --set baseDomain=${base_domain} \
-    front-end-development .
+    front-end-${environment} .
--- scripts-by-chapter/chapter-5.sh
+++ scripts-by-chapter/chapter-5.sh
@@ -2,7 +2,55 @@ echo "***************************************************"
 echo "********* CHAPTER 5 - STARTED AT $(date) **********"
 echo "***************************************************"
 
+# Updating Development
+    ( cd ./resource-api/infra/helm-v3 && ./create.sh )
+    ( cd ./clients-api/infra/helm-v3 && ./create.sh )
+    ( cd ./inventory-api/infra/helm-v3 && ./create.sh )
+    ( cd ./renting-api/infra/helm-v3 && ./create.sh )
+    ( cd ./front-end/infra/helm-v3 && ./create.sh )
 
+
+#  Create the Production DynamoDB Tables
+    ( cd ./clients-api/infra/cloudformation && ./create-dynamodb-table.sh production )
+    ( cd ./inventory-api/infra/cloudformation && ./create-dynamodb-table.sh production )
+    ( cd ./renting-api/infra/cloudformation && ./create-dynamodb-table.sh production )
+    ( cd ./resource-api/infra/cloudformation && ./create-dynamodb-table.sh production )
+
+
+# Create IAM Policies of Bookstore Microservices
+    ( cd clients-api/infra/cloudformation && ./create-iam-policy.sh production )
+    ( cd resource-api/infra/cloudformation && ./create-iam-policy.sh production )
+    ( cd inventory-api/infra/cloudformation && ./create-iam-policy.sh production )
+    ( cd renting-api/infra/cloudformation && ./create-iam-policy.sh production )
+
+# Create IAM Service Accounts
+    resource_iam_policy=$(aws cloudformation describe-stacks --stack production-iam-policy-resource-api --query "Stacks[0].Outputs[0]" | jq .OutputValue | tr -d '"')
+    renting_iam_policy=$(aws cloudformation describe-stacks --stack production-iam-policy-renting-api --query "Stacks[0].Outputs[0]" | jq .OutputValue | tr -d '"')
+    inventory_iam_policy=$(aws cloudformation describe-stacks --stack production-iam-policy-inventory-api --query "Stacks[0].Outputs[0]" | jq .OutputValue | tr -d '"')
+    clients_iam_policy=$(aws cloudformation describe-stacks --stack production-iam-policy-clients-api --query "Stacks[0].Outputs[0]" | jq .OutputValue | tr -d '"')
+    eksctl create iamserviceaccount --name resources-api-iam-service-account \
+        --namespace production \
+        --cluster eks-acg \
+        --attach-policy-arn ${resource_iam_policy} --approve
+    eksctl create iamserviceaccount --name renting-api-iam-service-account \
+        --namespace production \
+        --cluster eks-acg \
+        --attach-policy-arn ${renting_iam_policy} --approve
+    eksctl create iamserviceaccount --name inventory-api-iam-service-account \
+        --namespace production \
+        --cluster eks-acg \
+        --attach-policy-arn ${inventory_iam_policy} --approve
+    eksctl create iamserviceaccount --name clients-api-iam-service-account \
+        --namespace production \
+        --cluster eks-acg \
+        --attach-policy-arn ${clients_iam_policy} --approve
+
+# Installing the Production applications
+    ( cd ./resource-api/infra/helm-v3 && ./create.sh )
+    ( cd ./clients-api/infra/helm-v3 && ./create.sh )
+    ( cd ./inventory-api/infra/helm-v3 && ./create.sh )
+    ( cd ./renting-api/infra/helm-v3 && ./create.sh )
+    ( cd ./front-end/infra/helm-v3 && ./create.sh )
 
 echo "*************************************************************"
 echo "********* READY FOR CHAPTER 6 - FINISHED AT $(date) *********"
