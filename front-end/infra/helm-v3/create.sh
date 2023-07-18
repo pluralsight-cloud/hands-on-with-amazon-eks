e78d4886ce9afb5e4157a246c9c2ed1c25a0c9b6
mariomerco <mario.mercado.c@gmail.com>
2023-07-18 16:33:40 -0500

    front end helm v3 recovered

---
+++ front-end/infra/helm-v3/create.sh
@@ -0,0 +1,14 @@
+environment=${1}
+
+if [ -z "$environment" ]; then
+    echo "No environment specified. Using 'development'"
+    environment='development'
+fi
+
+base_domain=$(aws route53 list-hosted-zones --query "HostedZones[0].Name" --output text | rev | cut -c2- | rev)
+
+helm upgrade --install \
+    --namespace ${environment} \
+    --create-namespace \
+    --set baseDomain=${base_domain} \
+    front-end-${environment} .
