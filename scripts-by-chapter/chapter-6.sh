be3bbb4f6372a7c5bd079f2f86f97dec0f880fbc
mariomerco <mario.mercado.c@gmail.com>
2023-07-21 13:41:22 -0500

    chapter 6 looking good

--- scripts-by-chapter/chapter-6.sh
+++ scripts-by-chapter/chapter-6.sh
@@ -15,6 +15,23 @@ echo "***************************************************"
     kubectl label namespace development mesh=development-mesh
     kubectl label namespace development "appmesh.k8s.aws/sidecarInjectorWebhook"=enabled
 
+# Install Helm Charts with Mesh Components
+
+    front_end_image_tag=$(aws ecr list-images --repository-name bookstore.front-end --query "imageIds[0].imageTag" --output text | xargs)
+    clients_api_image_tag=$(aws ecr list-images --repository-name bookstore.clients-api --query "imageIds[0].imageTag" --output text | xargs)
+    renting_api_image_tag=$(aws ecr list-images --repository-name bookstore.renting-api --query "imageIds[0].imageTag" --output text | xargs)
+    resource_api_image_tag=$(aws ecr list-images --repository-name bookstore.resource-api --query "imageIds[0].imageTag" --output text | xargs)
+    inventory_api_image_tag=$(aws ecr list-images --repository-name bookstore.inventory-api --query "imageIds[0].imageTag" --output text | xargs)
+
+    ( cd ./resource-api/infra/helm-v6 && ./create.sh development ${resource_api_image_tag} ) & \
+    ( cd ./clients-api/infra/helm-v6 && ./create.sh development ${clients_api_image_tag} ) & \
+    ( cd ./inventory-api/infra/helm-v6 && ./create.sh development ${inventory_api_image_tag} ) & \
+    ( cd ./renting-api/infra/helm-v6 && ./create.sh development ${renting_api_image_tag} ) & \
+    ( cd ./front-end/infra/helm-v6 && ./create.sh development ${front_end_image_tag} ) &
+
+    wait
+
+
 echo "*************************************************************"
 echo "********* READY FOR CHAPTER 7 - FINISHED AT $(date) *********"
 echo "*************************************************************"
