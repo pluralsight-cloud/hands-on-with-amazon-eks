61d48c99cc993427f4359483f193514fa784055d
mariomerco <mario.mercado.c@gmail.com>
2023-07-21 07:52:40 -0500

    finishing production environment

--- scripts-by-chapter/chapter-5.sh
+++ scripts-by-chapter/chapter-5.sh
@@ -298,89 +298,89 @@ export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output t
 
     wait
 
-# # Create IAM Service Accounts
-#     resource_iam_policy=$(aws cloudformation describe-stacks --stack production-iam-policy-resource-api --query "Stacks[0].Outputs[0]" | jq .OutputValue | tr -d '"')
-#     renting_iam_policy=$(aws cloudformation describe-stacks --stack production-iam-policy-renting-api --query "Stacks[0].Outputs[0]" | jq .OutputValue | tr -d '"')
-#     inventory_iam_policy=$(aws cloudformation describe-stacks --stack production-iam-policy-inventory-api --query "Stacks[0].Outputs[0]" | jq .OutputValue | tr -d '"')
-#     clients_iam_policy=$(aws cloudformation describe-stacks --stack production-iam-policy-clients-api --query "Stacks[0].Outputs[0]" | jq .OutputValue | tr -d '"')
+# Create IAM Service Accounts
+    resource_iam_policy=$(aws cloudformation describe-stacks --stack production-iam-policy-resource-api --query "Stacks[0].Outputs[0]" | jq .OutputValue | tr -d '"')
+    renting_iam_policy=$(aws cloudformation describe-stacks --stack production-iam-policy-renting-api --query "Stacks[0].Outputs[0]" | jq .OutputValue | tr -d '"')
+    inventory_iam_policy=$(aws cloudformation describe-stacks --stack production-iam-policy-inventory-api --query "Stacks[0].Outputs[0]" | jq .OutputValue | tr -d '"')
+    clients_iam_policy=$(aws cloudformation describe-stacks --stack production-iam-policy-clients-api --query "Stacks[0].Outputs[0]" | jq .OutputValue | tr -d '"')
     
     
-#     eksctl create iamserviceaccount --name resources-api-iam-service-account \
-#         --namespace production \
-#         --cluster eks-acg \
-#         --attach-policy-arn ${resource_iam_policy} --approve & \
-#     eksctl create iamserviceaccount --name renting-api-iam-service-account \
-#         --namespace production \
-#         --cluster eks-acg \
-#         --attach-policy-arn ${renting_iam_policy} --approve & \
-#     eksctl create iamserviceaccount --name inventory-api-iam-service-account \
-#         --namespace production \
-#         --cluster eks-acg \
-#         --attach-policy-arn ${inventory_iam_policy} --approve & \
-#     eksctl create iamserviceaccount --name clients-api-iam-service-account \
-#         --namespace production \
-#         --cluster eks-acg \
-#         --attach-policy-arn ${clients_iam_policy} --approve &
+    eksctl create iamserviceaccount --name resources-api-iam-service-account \
+        --namespace production \
+        --cluster eks-acg \
+        --attach-policy-arn ${resource_iam_policy} --approve & \
+    eksctl create iamserviceaccount --name renting-api-iam-service-account \
+        --namespace production \
+        --cluster eks-acg \
+        --attach-policy-arn ${renting_iam_policy} --approve & \
+    eksctl create iamserviceaccount --name inventory-api-iam-service-account \
+        --namespace production \
+        --cluster eks-acg \
+        --attach-policy-arn ${inventory_iam_policy} --approve & \
+    eksctl create iamserviceaccount --name clients-api-iam-service-account \
+        --namespace production \
+        --cluster eks-acg \
+        --attach-policy-arn ${clients_iam_policy} --approve &
 
-#     wait
+    wait
 
-# # Installing the Production applications
+# Installing the Production applications
 
-#     sleep 300 # wait until everything has images. More or less, 5 minutes
+    sleep 300 # wait until everything has images. More or less, 5 minutes
 
-#     front_end_image_tag=$(aws ecr list-images --repository-name bookstore.front-end --query "imageIds[0].imageTag" --output text | xargs)
-#     clients_api_image_tag=$(aws ecr list-images --repository-name bookstore.clients-api --query "imageIds[0].imageTag" --output text | xargs)
-#     renting_api_image_tag=$(aws ecr list-images --repository-name bookstore.renting-api --query "imageIds[0].imageTag" --output text | xargs)
-#     resource_api_image_tag=$(aws ecr list-images --repository-name bookstore.resource-api --query "imageIds[0].imageTag" --output text | xargs)
-#     inventory_api_image_tag=$(aws ecr list-images --repository-name bookstore.inventory-api --query "imageIds[0].imageTag" --output text | xargs)
+    front_end_image_tag=$(aws ecr list-images --repository-name bookstore.front-end --query "imageIds[0].imageTag" --output text | xargs)
+    clients_api_image_tag=$(aws ecr list-images --repository-name bookstore.clients-api --query "imageIds[0].imageTag" --output text | xargs)
+    renting_api_image_tag=$(aws ecr list-images --repository-name bookstore.renting-api --query "imageIds[0].imageTag" --output text | xargs)
+    resource_api_image_tag=$(aws ecr list-images --repository-name bookstore.resource-api --query "imageIds[0].imageTag" --output text | xargs)
+    inventory_api_image_tag=$(aws ecr list-images --repository-name bookstore.inventory-api --query "imageIds[0].imageTag" --output text | xargs)
 
-#     ( cd ./resource-api/infra/helm-v5 && ./create.sh production ${resource_api_image_tag} ) & \
-#     ( cd ./clients-api/infra/helm-v5 && ./create.sh production ${clients_api_image_tag} ) & \
-#     ( cd ./inventory-api/infra/helm-v5 && ./create.sh production ${inventory_api_image_tag} ) & \
-#     ( cd ./renting-api/infra/helm-v5 && ./create.sh production ${renting_api_image_tag} ) & \
-#     ( cd ./front-end/infra/helm-v5 && ./create.sh production ${front_end_image_tag} ) &
+    ( cd ./resource-api/infra/helm-v5 && ./create.sh production ${resource_api_image_tag} ) & \
+    ( cd ./clients-api/infra/helm-v5 && ./create.sh production ${clients_api_image_tag} ) & \
+    ( cd ./inventory-api/infra/helm-v5 && ./create.sh production ${inventory_api_image_tag} ) & \
+    ( cd ./renting-api/infra/helm-v5 && ./create.sh production ${renting_api_image_tag} ) & \
+    ( cd ./front-end/infra/helm-v5 && ./create.sh production ${front_end_image_tag} ) &
 
-#     wait
+    wait
 
-# # Automatic Deployment to Production Environment
+# Automatic Deployment to Production Environment
 
-#     ( cd Infrastructure/cloudformation/cicd && \
-#         aws cloudformation deploy \
-#             --stack-name inventory-api-codecommit-repo \
-#             --template-file cicd-5-deploy-prod.yaml \
-#             --capabilities CAPABILITY_IAM \
-#             --parameter-overrides \
-#                 AppName=inventory-api ) & \
-#     ( cd Infrastructure/cloudformation/cicd && \
-#         aws cloudformation deploy \
-#             --stack-name resource-api-codecommit-repo \
-#             --template-file cicd-5-deploy-prod.yaml \
-#             --capabilities CAPABILITY_IAM \
-#             --parameter-overrides \
-#                 AppName=resource-api ) & \
-#     ( cd Infrastructure/cloudformation/cicd && \
-#         aws cloudformation deploy \
-#             --stack-name renting-api-codecommit-repo \
-#             --template-file cicd-5-deploy-prod.yaml \
-#             --capabilities CAPABILITY_IAM \
-#             --parameter-overrides \
-#                 AppName=renting-api ) & \
-#     ( cd Infrastructure/cloudformation/cicd && \
-#         aws cloudformation deploy \
-#             --stack-name clients-api-codecommit-repo \
-#             --template-file cicd-5-deploy-prod.yaml \
-#             --capabilities CAPABILITY_IAM \
-#             --parameter-overrides \
-#                 AppName=clients-api ) & \
-#     ( cd Infrastructure/cloudformation/cicd && \
-#         aws cloudformation deploy \
-#             --stack-name front-end-codecommit-repo \
-#             --template-file cicd-5-deploy-prod.yaml \
-#             --capabilities CAPABILITY_IAM \
-#             --parameter-overrides \
-#                 AppName=front-end ) &
+    ( cd Infrastructure/cloudformation/cicd && \
+        aws cloudformation deploy \
+            --stack-name inventory-api-codecommit-repo \
+            --template-file cicd-5-deploy-prod.yaml \
+            --capabilities CAPABILITY_IAM \
+            --parameter-overrides \
+                AppName=inventory-api ) & \
+    ( cd Infrastructure/cloudformation/cicd && \
+        aws cloudformation deploy \
+            --stack-name resource-api-codecommit-repo \
+            --template-file cicd-5-deploy-prod.yaml \
+            --capabilities CAPABILITY_IAM \
+            --parameter-overrides \
+                AppName=resource-api ) & \
+    ( cd Infrastructure/cloudformation/cicd && \
+        aws cloudformation deploy \
+            --stack-name renting-api-codecommit-repo \
+            --template-file cicd-5-deploy-prod.yaml \
+            --capabilities CAPABILITY_IAM \
+            --parameter-overrides \
+                AppName=renting-api ) & \
+    ( cd Infrastructure/cloudformation/cicd && \
+        aws cloudformation deploy \
+            --stack-name clients-api-codecommit-repo \
+            --template-file cicd-5-deploy-prod.yaml \
+            --capabilities CAPABILITY_IAM \
+            --parameter-overrides \
+                AppName=clients-api ) & \
+    ( cd Infrastructure/cloudformation/cicd && \
+        aws cloudformation deploy \
+            --stack-name front-end-codecommit-repo \
+            --template-file cicd-5-deploy-prod.yaml \
+            --capabilities CAPABILITY_IAM \
+            --parameter-overrides \
+                AppName=front-end ) &
 
-#     wait
+    wait
 
 
 echo "*************************************************************"
