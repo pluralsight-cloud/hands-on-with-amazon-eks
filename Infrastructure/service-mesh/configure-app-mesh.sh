8b7c2c641be5dd45e66aa8309ab0a28eb174cf32
mariomerco <mario.mercado.c@gmail.com>
2023-07-21 13:32:48 -0500

    inventory api helm v6

--- Infrastructure/service-mesh/configure-app-mesh.sh
+++ Infrastructure/service-mesh/configure-app-mesh.sh
@@ -1,23 +1,31 @@
-helm repo add eks https://aws.github.io/eks-charts
-kubectl apply -k "https://github.com/aws/eks-charts/stable/appmesh-controller/crds?ref=master"
-kubectl create ns appmesh-system
+aws cloudformation deploy \
+    --stack-name appmesh-controller-iam-policy \
+    --template-file iam-policy.yaml \
+    --capabilities CAPABILITY_NAMED_IAM
+
+account_id=$(aws sts get-caller-identity --query "Account" --output text | xargs)    
+appmesh_controller_policy_arn="arn:aws:iam::${account_id}:policy/AppMeshControllerPolicy"
 
 export CLUSTER_NAME=eks-acg
-export AWS_REGION=us-east-2
+export AWS_REGION=us-east-1
 
 eksctl create iamserviceaccount \
-    --cluster $CLUSTER_NAME \
+    --cluster eks-acg \
     --namespace appmesh-system \
     --name appmesh-controller \
-    --attach-policy-arn  arn:aws:iam::aws:policy/AWSCloudMapFullAccess,arn:aws:iam::aws:policy/AWSAppMeshFullAccess,arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess \
-    --override-existing-serviceaccounts \
+    --attach-policy-arn ${appmesh_controller_policy_arn} \
     --approve
+
+helm repo add eks https://aws.github.io/eks-charts
+kubectl apply -k "https://github.com/aws/eks-charts/stable/appmesh-controller/crds?ref=master"
+# kubectl create ns appmesh-system
+
 
 helm upgrade -i appmesh-controller eks/appmesh-controller \
     --namespace appmesh-system \
     --set region=$AWS_REGION \
     --set serviceAccount.create=false \
     --set serviceAccount.name=appmesh-controller \
-    --set log.level=debug \
-    --set tracing.enabled=true \
-    --set tracing.provider=x-ray
+    --set log.level=debug
+    # --set tracing.enabled=true \
+    # --set tracing.provider=x-ray
--- scripts-by-chapter/chapter-6.sh
+++ scripts-by-chapter/chapter-6.sh
@@ -2,7 +2,18 @@ echo "***************************************************"
 echo "********* CHAPTER 6 - STARTED AT $(date) **********"
 echo "***************************************************"
 
+# Install the AppMesh Controller
 
+    ( cd ./Infrastructure/service-mesh && ./configure-app-mesh.sh )
+
+# Create the Development and Production App Meshes
+
+    ( cd ./Infrastructure/service-mesh && kubectl apply -f development-mesh.yaml )
+    ( cd ./Infrastructure/service-mesh && kubectl apply -f production-mesh.yaml )
+
+# Enable Namespace
+    kubectl label namespace development mesh=development-mesh
+    kubectl label namespace development "appmesh.k8s.aws/sidecarInjectorWebhook"=enabled
 
 echo "*************************************************************"
 echo "********* READY FOR CHAPTER 7 - FINISHED AT $(date) *********"
