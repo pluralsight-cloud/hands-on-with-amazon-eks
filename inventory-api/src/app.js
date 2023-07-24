b136502db7ae6e19f43512ce34d3321f875eac90
mariomerco <mario.mercado.c@gmail.com>
2023-07-24 15:42:23 -0500

    changing default region

--- inventory-api/src/app.js
+++ inventory-api/src/app.js
@@ -23,18 +23,18 @@ app.use(AWSXRay.express.openSegment('Inventory API'));
 
 const port = 5001;
 const resourceApiEndpont = process.env.RESOURCE_API_ENDPOINT || 'http://localhost:5000';
-const dynamodbTable = process.env.DYNAMODB_TABLE || 'development-inventory' ;
-const awsDefaultRegion = process.env.AWS_DEFAULT_REGION || 'us-east-2'
-const dynamoDb = new AWS.DynamoDB.DocumentClient({region: awsDefaultRegion});
+const dynamodbTable = process.env.DYNAMODB_TABLE || 'development-inventory';
+const awsDefaultRegion = process.env.AWS_DEFAULT_REGION || 'us-east-1'
+const dynamoDb = new AWS.DynamoDB.DocumentClient({ region: awsDefaultRegion });
 
 app.use(AWSXRay.express.openSegment('Inventory API'));
 
 app.post('/register', (request, response) => {
     inventoryRegistryJson = request.body;
     // TODO: Validate
-    
+
     resourceApiUrl = resourceApiEndpont + '/get/' + inventoryRegistryJson.ResourceId;
-    
+
     client = resourceApiUrl.startsWith("http://") ? http_client : https_client;
 
     resourceApiResponse = client.get(resourceApiUrl, (resp) => {
@@ -44,33 +44,33 @@ app.post('/register', (request, response) => {
             data += chunk;
         });
         resp.on('end', () => {
-            if(inventoryRegistryJson.Available == undefined) {
+            if (inventoryRegistryJson.Available == undefined) {
                 inventoryRegistryJson.Available = true
             }
 
             inventoryRegistryJson._id = uuidv1();
-            
+
             var params = {
                 TableName: dynamodbTable,
                 Item: inventoryRegistryJson
             }
 
-            dynamoDb.put(params, function(err, data){
-                if(err) {
+            dynamoDb.put(params, function (err, data) {
+                if (err) {
                     console.log(err)
                     response.status(500)
                         .json({ success: false, message: 'Error: Server error' });
-                } else{
+                } else {
                     console.log('data', data);
                     response.status(201)
                         .json({ 'insertedId': inventoryRegistryJson._id })
                 }
-            })    
+            })
         });
         resp.on('error', (err) => {
-            if(resp.statusCode == 404) {
+            if (resp.statusCode == 404) {
                 response.status(404)
-                .json({'message': "The resource wasn't found."})
+                    .json({ 'message': "The resource wasn't found." })
             } else {
                 console.log(err)
             }
@@ -83,18 +83,18 @@ app.get('/list/:resourceId', async (request, response) => {
     var isAvailableArg = request.query.available
     var isAvailable = true
 
-    if(isAvailableArg != undefined) {
+    if (isAvailableArg != undefined) {
         isAvailable = (isAvailableArg == 'true')
     }
     var params = {
         TableName: dynamodbTable,
         ExpressionAttributeValues: {
             ":r": id,
-            ":a": isAvailable 
+            ":a": isAvailable
         },
         FilterExpression: "ResourceId = :r and Available = :a"
     }
-    dynamoDb.scan(params, function(err,data) {
+    dynamoDb.scan(params, function (err, data) {
         console.log('data', data)
         response.json(data)
     })
@@ -111,18 +111,18 @@ app.put('/setAvailability/:id', async (request, response) => {
         },
         UpdateExpression: "SET #available = :a",
         ExpressionAttributeValues: {
-            ":a": isAvailable 
+            ":a": isAvailable
         },
         ExpressionAttributeNames: {
-            "#available": "Available" 
+            "#available": "Available"
         }
     }
-    dynamoDb.update(params, function(err,data) {
+    dynamoDb.update(params, function (err, data) {
         response.json({ "message": "Updated successully" })
     })
 })
 
-app.get('/summary', function(request, response) {
+app.get('/summary', function (request, response) {
     resourceApiListUrl = resourceApiEndpont + "/list"
     console.log(resourceApiListUrl)
 
@@ -136,19 +136,19 @@ app.get('/summary', function(request, response) {
         resp.on('end', () => {
             console.log(data)
             let listOfResources = JSON.parse(data)
-            const r = iterateResponsePromise(listOfResources, response)      
+            const r = iterateResponsePromise(listOfResources, response)
         });
         resp.on('error', (err) => {
             console.log(err)
         });
-        
+
     })
 })
 
 app.use(AWSXRay.express.closeSegment());
 
 async function iterateResponsePromise(listOfResources, response) {
-    async.map(listOfResources.data.data, function(item, callback) {
+    async.map(listOfResources.data.data, function (item, callback) {
         // Set ID
         var tempObject = {
             ResourceId: item._id,
@@ -162,12 +162,12 @@ async function iterateResponsePromise(listOfResources, response) {
             TableName: dynamodbTable,
             ExpressionAttributeValues: {
                 ":r": item._id,
-                ":a": true 
+                ":a": true
             },
             FilterExpression: "ResourceId = :r and Available = :a"
         }
-        dynamoDb.scan(params, (err,data) => {
-            
+        dynamoDb.scan(params, (err, data) => {
+
             tempObject.AvailableCopies = data.Count;
 
             var paramsUnavailable = {
@@ -178,15 +178,15 @@ async function iterateResponsePromise(listOfResources, response) {
                 },
                 FilterExpression: "ResourceId = :r and Available = :a"
             }
-            dynamoDb.scan(paramsUnavailable, (err,data) => {
+            dynamoDb.scan(paramsUnavailable, (err, data) => {
                 tempObject.UnavailableCopies = data.Count;
                 tempObject.TotalCopies = tempObject.AvailableCopies + tempObject.UnavailableCopies;
                 callback(null, tempObject);
             })
         })
 
-    }, function(err, results){
-        if(err) return console.log(err);
+    }, function (err, results) {
+        if (err) return console.log(err);
         response.json(results)
     })
 }
