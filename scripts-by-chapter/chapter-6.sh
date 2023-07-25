9a1d20e8262f3b6b76e66a4cebc425d990e6826a
mariomerco <mario.mercado.c@gmail.com>
2023-07-25 17:07:53 -0500

    ms 2000

--- scripts-by-chapter/chapter-6.sh
+++ scripts-by-chapter/chapter-6.sh
@@ -108,6 +108,20 @@ echo "***************************************************"
 
     # kubectl scale deploy -n development resource-api-development-acg-resource-api --replicas=0
     
+    front_end_image_tag=$(aws ecr list-images --repository-name bookstore.front-end --query "imageIds[0].imageTag" --output text | xargs)
+    clients_api_image_tag=$(aws ecr list-images --repository-name bookstore.clients-api --query "imageIds[0].imageTag" --output text | xargs)
+    renting_api_image_tag=$(aws ecr list-images --repository-name bookstore.renting-api --query "imageIds[0].imageTag" --output text | xargs)
+    resource_api_image_tag=$(aws ecr list-images --repository-name bookstore.resource-api --query "imageIds[0].imageTag" --output text | xargs)
+    inventory_api_image_tag=$(aws ecr list-images --repository-name bookstore.inventory-api --query "imageIds[0].imageTag" --output text | xargs)
+
+    ( cd ./resource-api/infra/helm-v7 && ./create.sh development ${resource_api_image_tag} ) & \
+    ( cd ./clients-api/infra/helm-v7 && ./create.sh development ${clients_api_image_tag} ) & \
+    ( cd ./inventory-api/infra/helm-v7 && ./create.sh development ${inventory_api_image_tag} ) & \
+    ( cd ./renting-api/infra/helm-v7 && ./create.sh development ${renting_api_image_tag} ) & \
+    ( cd ./front-end/infra/helm-v7 && ./create.sh development ${front_end_image_tag} ) &
+
+    wait
+
 
 echo "*************************************************************"
 echo "********* READY FOR CHAPTER 7 - FINISHED AT $(date) *********"
