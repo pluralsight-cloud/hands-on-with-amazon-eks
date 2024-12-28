e3c01a58efcdef4ea2eae4a4b386864a7399081d
dejan2609 <dejan2609@gmail.com>
2024-12-28 15:33:02 +0100

    eksctl version upgraded (0.147 -->> 0.199) in order to prevent errors (while creating cluster via `eksctl create cluster -f /Infrastructure/eksctl/01-initial-cluster/cluster.yaml`)

--- scripts-by-chapter/prepare-cloud-shell.sh
+++ scripts-by-chapter/prepare-cloud-shell.sh
@@ -2,7 +2,7 @@ echo "---------- INSTALLING NANO ----------"
 sudo yum install nano -y
 
 echo "---------- INSTALLING EKSCTL ----------"
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/v0.147.0/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/v0.199.0/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
 
